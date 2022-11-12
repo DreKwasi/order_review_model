a8ff50e27f161b2b333ff4f17c79a761cef531e4
DreKwasi <andrewsboateng137@gmail.com>
2022-11-12 17:48:56 +0000

    Description of Application and Functions.

--- st_app/about.py
+++ st_app/about.py
@@ -6,63 +6,29 @@ def show_page():
     # About the GDELT Project
     with st.expander("About this App"):
         st.markdown("""
-        The GDELT Project monitors the world's broadcast, print and web news around
-        the globe in over 100 languages. It identifies people, locations, organizations,
-        themes, emotions, events and other entities that continuously drive our society.
+        The Order Review Project monitors the month on month orders from Retail facilities.
+        
+        Given the fulfillment strain on the Supply Chain Unit, the Order Review Model provides a unique
+        perspective on how orders should be processed.
 
-        Given its global reach, the GDELT Project is a unique resource which allows us
-        to gain a global perspective on what is happening and how the world feels about it.
-        The GDELT archives are constantly updated every 15 minutes. The data is
-        fully public and is available via Google BigQuery, and via GDELT's own FTP servers.
-
-        Learn more about the GDELT Project [here](https://gdeltproject.org/about/).
+        The Order Review Model employs the Stock Balance, Product Category, Facility & Regional Dispensation of Products
+        and in future models the Withdrawal Pattern 
         """)
 
     # About the data we are using
-    with st.expander("The Events dataset"):
+    with st.expander("The Dataset"):
         st.markdown("""
-        This app uses the GDELT 2.0 Events dataset. It is a collection of worldwide
-        activities, i.e. events, in over 300 categories such as diplomatic exchanges,
-        natural disasters, or any other significant event of relevance. Each event
-        record comprises over 50 fields capturing many different aspects of the event.
-        The dataset is continuously updated every 15 minutes. The snapshot used in this app
-        ranges from February 2014 until April 2022, and comprises a little over 625 million events.
+        This app uses Metabase Data from the flagship POS Software.
 
         Learn more about this dataset [here](https://blog.gdeltproject.org/gdelt-2-0-our-global-world-in-realtime/).
         """)
 
-    # What are Actors?
-    with st.expander("What are Actors?"):
-        st.markdown("""
-        Actors are the entities that are involved in the events captured by the GDELT dataset.
-        Actors can be people, organizations, countries, or any other entities or objects that
-        were the main topic of the news repots. The GDELT 2.0 Events dataset contains various
-        attributes related to the actors.
-
-        This app focuses on the actor type attributes, which
-        are 3-character codes that describe the "type" or "role" of the actor. Example of such
-        types or roles are "Police Force", "Government", "Non-governmental Organizations".
-
-        Learn more about the actor attributes [here](http://data.gdeltproject.org/documentation/GDELT-Event_Codebook-V2.0.pdf).
-        """)
-
-    # Actor Code Types definition
-    with st.expander("Actor code types definitions"):
-        code_definitions_md = ''
-        for code in actor_codes.keys():
-            code_definitions_md += f" - {code}: {actor_codes[code]}\n"
-        st.markdown(code_definitions_md)
-
     # How to use the app
     with st.expander("How to use the app"):
         st.markdown("""
-        With this app you can explore different actor types, and see how the asscoiated news impact the world.
-        Simply choose one or more types in the side panel on the left, and you will be presented with:
-         - General statistics about the news articles related to these actor types
-         - Trends of the [GoldStein scale](http://web.pdx.edu/~kinsella/jgscale.html) and the average tone (sentiment) of the selected articles
-         - Map of the world showing showing where the actors are coming from and how the news impact each country
-         - A word-cloud of the 300 most common actor names.
-
+        With this app you can explore the various effects of:
+         - 
+         
         Optionally you can also constrain the time period for which you are interested in.
         """)
 
