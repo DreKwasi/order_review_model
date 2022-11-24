880008ad48a8f0355fa4bfd1205acb91de1f832c
DreKwasi <andrewsboateng137@gmail.com>
2022-11-24 12:22:31 +0000

    Changed Column Names

--- st_app/order_review.py
+++ st_app/order_review.py
@@ -39,11 +39,11 @@ def merge_order(df, vx_df, facility, location, date_range):
     stock_df = stk_df[stk_df['facility_name'] == facility]
 
     facility_gp = facility_df.groupby(
-        by=['Product_Description'], agg={"Packs_Sold": "sum"})
+        by=['Product_Description'], agg={"Sum_of_Quantity_In_Packs": "sum"})
     facility_gp = facility_gp.to_pandas_df()
 
     regional_gp = location_df.groupby(
-        by=['Product_Description'], agg={"Packs_Sold": "sum"})
+        by=['Product_Description'], agg={"Sum_of_Quantity_In_Packs": "sum"})
     regional_gp = regional_gp.to_pandas_df()
 
     df['Packs Dispensed (Facility Level)'] = 0
@@ -53,52 +53,52 @@ def merge_order(df, vx_df, facility, location, date_range):
 
     for index, row in df.iterrows():
         facility_match = facility_gp[facility_gp['Product_Description']
-                                     == row['DRUG ']]
-        df.loc[index, 'Packs Dispensed (Facility Level)'] = facility_match['Packs_Sold'].values[0] if len(
-            facility_match['Packs_Sold'].values) > 0 else 0
+                                     == row['PRODUCT']]
+        df.loc[index, 'Packs Dispensed (Facility Level)'] = facility_match['Sum_of_Quantity_In_Packs'].values[0] if len(
+            facility_match['Sum_of_Quantity_In_Packs'].values) > 0 else 0
 
         region_match = regional_gp[regional_gp['Product_Description']
-                                   == row['DRUG ']]
-        df.loc[index, 'Packs Dispensed (Regional Level)'] = region_match['Packs_Sold'].values[0] if len(
-            region_match['Packs_Sold'].values) > 0 else 0
+                                   == row['PRODUCT']]
+        df.loc[index, 'Packs Dispensed (Regional Level)'] = region_match['Sum_of_Quantity_In_Packs'].values[0] if len(
+            region_match['Sum_of_Quantity_In_Packs'].values) > 0 else 0
 
-        stock_match = stock_df[stock_df['product_name'] == row['DRUG ']]
+        stock_match = stock_df[stock_df['product_name'] == row['PRODUCT']]
         df.loc[index, 'Stock Balance (Facility Level)'] = stock_match['Stock Balance'].values[0] if len(
             stock_match['Stock Balance'].values) > 0 else 0
 
 
 def compute_final(df):
-    df['Final Requested Quantity'] = 0
+    df['REVIEWED QTY'] = 0
     df['Excess Variance'] = 0
 
     for index, row in df.iterrows():
-        if row['Packs Dispensed (Facility Level)'] < row['QUANTITY ']:
+        if row['Packs Dispensed (Facility Level)'] < row['REQUESTED QTY']:
             dispensation_factor = (
-                row['Packs Dispensed (Facility Level)'] / row['QUANTITY ']) * 0.5
+                row['Packs Dispensed (Facility Level)'] / row['REQUESTED QTY']) * 0.5
         else:
             dispensation_factor = 0.5
 
-        if row['Packs Dispensed (Regional Level)'] < row['QUANTITY ']:
+        if row['Packs Dispensed (Regional Level)'] < row['REQUESTED QTY']:
             location_factor = (
-                row['Packs Dispensed (Regional Level)']) / row['QUANTITY '] * 0.25
+                row['Packs Dispensed (Regional Level)']) / row['REQUESTED QTY'] * 0.25
         else:
             location_factor = 0.25
 
-        perm_stk = row['QUANTITY '] * 0.25
+        perm_stk = row['REQUESTED QTY'] * 0.25
         if row['Stock Balance (Facility Level)'] > perm_stk:
             balance = row['Stock Balance (Facility Level)'] - perm_stk
-            if balance > row['QUANTITY ']:
+            if balance > row['REQUESTED QTY']:
                 stock_factor = 0
             else:
-                stock_factor = (1 - (balance / row['QUANTITY '])) * 0.25
+                stock_factor = (1 - (balance / row['REQUESTED QTY'])) * 0.25
         else:
             stock_factor = 0.25
         total_factor = dispensation_factor + location_factor + stock_factor
-        final_qty = np.round(total_factor * row['QUANTITY '], decimals=0)
-        df.loc[index, 'Final Requested Quantity'] = 1 if final_qty < 1 else final_qty
+        final_qty = np.round(total_factor * row['REQUESTED QTY'], decimals=0)
+        df.loc[index, 'REVIEWED QTY'] = 1 if final_qty < 1 else final_qty
 
-    df['Total After Review'] = df['Final Requested Quantity'] * df['PRICE']
-    df['Excess Variance'] = np.round(df['QUANTITY '] - df['Final Requested Quantity'], decimals=2)
+    df['Total After Review'] = df['REVIEWED QTY'] * df['PRICE']
+    df['Excess Variance'] = np.round(df['REQUESTED QTY'] - df['REVIEWED QTY'], decimals=2)
 
 
 def convert_df(df):
@@ -136,6 +136,7 @@ def show_page():
 
     if file and facility:
         new_df = pd.read_csv(file)
+        new_df.dropna(inplace=True)
         merge_order(df=new_df, vx_df=vx_df,
                     facility=facility[0], location=location[0], date_range=date_range)
         new_df = new_df.round(decimals=2)
@@ -146,7 +147,7 @@ def show_page():
         metrics[0].metric(label='Total Order Value',
                           value=f"GHS {human_format(sum(new_df['TOTAL']))}")
         metrics[1].metric(label='Total Packs Requested',
-                          value=f"{human_format(sum(new_df['QUANTITY ']))}")
+                          value=f"{human_format(sum(new_df['REQUESTED QTY']))}")
         st.subheader("Order Data")
         with st.expander("View Raw Data"):
             st.dataframe(new_df)
@@ -162,12 +163,12 @@ def show_page():
             "Dispensed Sales": "Packs Dispensed (Facility Level)",
             
         }
-        chart_y = ["QUANTITY ", ]
+        chart_y = ["REQUESTED QTY", ]
 
         for option in order_chart_options:
             chart_y.append(order_cols[option])
 
-        fig_1 = px.bar(df, x="DRUG ", y=chart_y, barmode='group')
+        fig_1 = px.bar(df, x="PRODUCT", y=chart_y, barmode='group')
         with fig_1.batch_update():
             fig_1.update_layout(coloraxis_showscale=False)
             fig_1.update_layout(width=1000, height=700, title="Comparing Order Against Various Metrics")
@@ -183,13 +184,13 @@ def show_page():
             st.subheader("Review Order Data")
 
             csv = convert_df(df)
-            st.download_button("Press to Download", csv, "file.csv", "text/csv", key='download-csv')
+            st.download_button("Press to Download", csv, f"{facility[0]}.csv", "text/csv", key='download-csv')
 
             with st.expander("Expand Data"):
                 metrics[2].metric(label='Total Order Value After Review',
                                   value=f"GHS {human_format(sum(df['Total After Review']))}")
                 metrics[3].metric(label='Total Packs After Review',
-                                  value=f"{human_format(sum(df['Final Requested Quantity']))}")
+                                  value=f"{human_format(sum(df['REVIEWED QTY']))}")
 
                 st.dataframe(df)
 
@@ -200,15 +201,15 @@ def show_page():
                 "Stock Balance": "Stock Balance (Facility Level)",
                 "Regional Sales": "Packs Dispensed (Regional Level)",
                 "Dispensed Sales": "Packs Dispensed (Facility Level)",
-                "Reviewed Quantity": "Final Requested Quantity",
+                "Reviewed Quantity": "REVIEWED QTY",
                 "Excess Variance" : "Excess Variance"
             }
-            bar_y = ["QUANTITY ", ]
+            bar_y = ["REQUESTED QTY", ]
 
             for option in compare_option:
                 bar_y.append(compare[option])
 
-            fig = px.bar(df, x="DRUG ", y=bar_y, barmode='group')
+            fig = px.bar(df, x="PRODUCT", y=bar_y, barmode='group')
             with fig.batch_update():
                 fig.update_layout(coloraxis_showscale=False)
                 fig.update_layout(width=1000, height=700, title="Comparing Order Against Various Metrics")
