2d00debc9da1cca01931b1415ecaa70cec276960
DreKwasi <andrewsboateng137@gmail.com>
2022-11-25 07:40:11 +0000

    Jupyter notebook for Cleanup and forecasting

---
+++ notebook_analysis/dtypes.py
@@ -0,0 +1,13 @@
+# Specifying all dtypes to maximize  efficiency
+dtypes = {
+    'Sale Date': 'str',
+    'Sale Facility': 'str',
+    'Customer Type': 'str',
+    'Vdl Drug ID': 'str',
+    'Vdl Drug Display Name': 'str',
+    'Product Name': 'str',
+    'Item Sub Category': 'str',
+    'Sum of Quantity In Units': 'int32',
+    'Sum of Quantity In Packs': 'float32'
+}
+
